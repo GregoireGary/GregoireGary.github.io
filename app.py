0bad2a8795b8f2b52ce66c31233fc50265694d6a
Grégoire Gary <ggary@proginov.com>
2023-10-11 00:05:18 +0200

    tableau entier

--- app.py
+++ app.py
@@ -1,25 +1,39 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, jsonify
+import json
 
 app = Flask(__name__, static_url_path='/static')
 
+def load_data_from_json():
+    try:
+        with open('data/data.json', 'r') as json_file:
+            data = json.load(json_file)
+        return data
+    except FileNotFoundError:
+        # Si le fichier n'existe pas encore, retournez un dictionnaire vide
+        return {}
 
-# Stockage des disponibilités des professeurs (c'est un exemple, vous pouvez utiliser une base de données)
-professors_availability = []
+# Charger les données au démarrage de l'application
+loaded_data = load_data_from_json()
 
 @app.route('/')
 def index():
-    return render_template('index.html')
+    return render_template('index.html', data=loaded_data)
 
-@app.route('/submit', methods=['POST'])
-def submit():
-    # Récupérer les données du formulaire
-    data = request.form.to_dict(flat=False)
+@app.route('/update', methods=['POST'])
+def update():
+    global loaded_data
+    updated_data = request.get_json()
+    
+    # Mettre à jour les données chargées avec les nouvelles données
+    loaded_data = updated_data
+    
+    # Écrivez les données dans le fichier JSON
+    with open('data/data.json', 'w') as json_file:
+        json.dump(updated_data, json_file)
 
-    # Traiter les données et les ajouter à la liste professors_availability
-    professors_availability.append(data)
-
-    # Rediriger vers la page d'accueil ou afficher un message de confirmation
-    return "Données enregistrées avec succès!"
+    # Répondre avec un message de succès
+    return jsonify({'message': 'Données mises à jour avec succès!'})
 
 if __name__ == '__main__':
     app.run(debug=True)
+
--- static/script.js
+++ static/script.js
@@ -1,5 +1,7 @@
-// script.js
+
+// Récupérez le corps du tableau où les lignes seront ajoutées
 var tableBody = document.getElementById("professorsTableBody");
+
 // Fonction pour ajouter une nouvelle ligne au tableau
 function addRow() {
     // Créez une nouvelle ligne et des cellules
@@ -11,15 +13,46 @@ function addRow() {
     var cell5 = document.createElement("td");
     var cell6 = document.createElement("td");
     var cell7 = document.createElement("td");
+    var cell8 = document.createElement("td");
+    var cell9 = document.createElement("td");
+    var cell10 = document.createElement("td");
+    var cell11 = document.createElement("td");
+    var cell12 = document.createElement("td");
+    var cell13 = document.createElement("td");
+    var cell14 = document.createElement("td");
+    var cell15 = document.createElement("td");
+    var cell16 = document.createElement("td");
+    var cell17 = document.createElement("td");
+    var cell18 = document.createElement("td");
+    var cell19 = document.createElement("td");
+    var cell20 = document.createElement("td");
+    var cell21 = document.createElement("td");
+    var cell22 = document.createElement("td");
 
-    // Ajoutez du contenu aux cellules (par exemple, des champs de formulaire)
+
+    // Ajoutez du contenu aux cellules (champs de formulaire, cases à cocher, etc.)
     cell1.innerHTML = '<input type="text" name="professor_name[]">';
-    cell2.innerHTML = '<select name="professor_subject[]"><option value="math">Math</option><option value="science">Science</option></select>';
-    cell3.innerHTML = '<input type="checkbox" name="class_a[]">';
-    cell4.innerHTML = '<input type="checkbox" name="class_b[]">';
-    cell5.innerHTML = '<input type="checkbox" name="class_c[]">';
-    cell6.innerHTML = '<input type="checkbox" name="monday_session_1[]">';
-    cell7.innerHTML = '<input type="checkbox" name="monday_session_2[]">';
+    cell2.innerHTML = '<select name="professor_subject[]"><option value="math">Math</option><option value="frs">Frs</option><option value="SC">SC</option><option value="ang">ang</option><option value="HG">HG</option><option value="art">art</option><option value="EPS">EPS</option><option value="LV2">LV2</option></select>';
+    cell3.innerHTML = '<select name="professor_PP[]"><option value="-">-</option><option value="61">61</option><option value="62">62</option><option value="63">63</option><option value="64">64</option><option value="51">51</option><option value="52">52</option><option value="53">53</option><option value="54">54</option><option value="41">41</option><option value="42">42</option><option value="43">43</option><option value="31">31</option><option value="32">32</option></select>';
+    cell4.innerHTML = '<input type="checkbox" name="61[]">';
+    cell5.innerHTML = '<input type="checkbox" name="62[]">';
+    cell6.innerHTML = '<input type="checkbox" name="63[]">';
+    cell7.innerHTML = '<input type="checkbox" name="64[]">';
+    cell8.innerHTML = '<input type="checkbox" name="51[]">';
+    cell9.innerHTML = '<input type="checkbox" name="52[]">';
+    cell10.innerHTML = '<input type="checkbox" name="53[]">';
+    cell11.innerHTML = '<input type="checkbox" name="54[]">';
+    cell12.innerHTML = '<input type="checkbox" name="41[]">';
+    cell13.innerHTML = '<input type="checkbox" name="42[]">';
+    cell14.innerHTML = '<input type="checkbox" name="43[]">';
+    cell15.innerHTML = '<input type="checkbox" name="31[]">';
+    cell16.innerHTML = '<input type="checkbox" name="32[]">';
+    cell17.innerHTML = '<input type="checkbox" name="L1[]">';
+    cell18.innerHTML = '<input type="checkbox" name="L2[]">';
+    cell19.innerHTML = '<input type="checkbox" name="M1[]">';
+    cell20.innerHTML = '<input type="checkbox" name="M2[]">';
+    cell21.innerHTML = '<input type="checkbox" name="J1[]">';
+    cell22.innerHTML = '<input type="checkbox" name="J2[]">';
 
     // Ajoutez les cellules à la nouvelle ligne
     newRow.appendChild(cell1);
@@ -29,6 +62,21 @@ function addRow() {
     newRow.appendChild(cell5);
     newRow.appendChild(cell6);
     newRow.appendChild(cell7);
+    newRow.appendChild(cell8);
+    newRow.appendChild(cell9);
+    newRow.appendChild(cell10);
+    newRow.appendChild(cell11);
+    newRow.appendChild(cell12);
+    newRow.appendChild(cell13);
+    newRow.appendChild(cell14);
+    newRow.appendChild(cell15);
+    newRow.appendChild(cell16);
+    newRow.appendChild(cell17);
+    newRow.appendChild(cell18);
+    newRow.appendChild(cell19);
+    newRow.appendChild(cell20);
+    newRow.appendChild(cell21);
+    newRow.appendChild(cell22);
 
     // Ajoutez la nouvelle ligne au corps du tableau
     tableBody.appendChild(newRow);
@@ -39,7 +87,86 @@ document.getElementById("addRowButton").addEventListener("click", addRow);
 
 document.getElementById("availabilityForm").addEventListener("submit", function(event) {
     event.preventDefault();
-    // Code JavaScript pour récupérer les données du formulaire et les envoyer au backend
-    // ...
 
+    // Récupérer toutes les lignes du tableau
+    var rows = document.querySelectorAll("#professorsTableBody tr");
+
+    // Initialiser un tableau pour stocker les données mises à jour
+    var updatedData = [];
+
+    // Parcourir chaque ligne et mettre à jour l'objet `data`
+    rows.forEach(function(row) {
+        var professorName = row.querySelector('input[name="professor_name[]"]').value;
+        var professorSubject = row.querySelector('select[name="professor_subject[]"]').value;
+        var professorPP = row.querySelector('select[name="professor_PP[]"]').value;
+        // Récupérer d'autres valeurs de champ ici...
+
+        var c61 = row.querySelector('input[name="61[]"]').checked;
+        var c62 = row.querySelector('input[name="62[]"]').checked;
+        var c63 = row.querySelector('input[name="63[]"]').checked;
+        var c64 = row.querySelector('input[name="64[]"]').checked;
+        var c51 = row.querySelector('input[name="51[]"]').checked;
+        var c52 = row.querySelector('input[name="52[]"]').checked;
+        var c53 = row.querySelector('input[name="53[]"]').checked;
+        var c54 = row.querySelector('input[name="54[]"]').checked;
+        var c41 = row.querySelector('input[name="41[]"]').checked;
+        var c42 = row.querySelector('input[name="42[]"]').checked;
+        var c43 = row.querySelector('input[name="43[]"]').checked;
+        var c31 = row.querySelector('input[name="31[]"]').checked;
+        var c32 = row.querySelector('input[name="32[]"]').checked;
+        var L1 = row.querySelector('input[name="L1[]"]').checked;
+        var L2 = row.querySelector('input[name="L2[]"]').checked;
+        var M1 = row.querySelector('input[name="M1[]"]').checked;
+        var M2 = row.querySelector('input[name="M2[]"]').checked;
+        var J1 = row.querySelector('input[name="J1[]"]').checked;
+        var J2 = row.querySelector('input[name="J2[]"]').checked;
+
+        var updatedProfessor = {
+            name: professorName,
+            subject: professorSubject,
+            PP: professorPP,
+            c61: c61,
+            c62: c62,
+            c63: c63,
+            c64: c64,
+            c51: c51,
+            c52: c52,
+            c53: c53,
+            c54: c54,
+            c41: c41,
+            c42: c42,
+            c43: c43,
+            c31: c31,
+            c32: c32,            
+            L1: L1,
+            L2: L2,
+            M1: M1,
+            M2: M2,
+            J1: J1,
+            J2: J2
+        };
+
+        // Ajouter le professeur mis à jour au tableau `updatedData`
+        updatedData.push(updatedProfessor);
+    });
+
+    // Mettre à jour l'objet `data` avec les nouvelles données
+    var data = { professors: updatedData };
+
+    // Envoyer l'objet `data` au backend via une requête AJAX
+    fetch('/update', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(data)
+    })
+    .then(function(response) {
+        // Gérer la réponse du serveur ici (par exemple, afficher un message de succès)
+        console.log('Données mises à jour avec succès !');
+    })
+    .catch(function(error) {
+        // Gérer les erreurs de la requête ici
+        console.error('Erreur lors de la mise à jour des données :', error);
+    });
 });
